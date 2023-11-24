137e29d6cc09148e38bcff5131b2b7b7113a669e
Maksmarts <150959375+Maksmarts@users.noreply.github.com>
2023-11-24 23:42:28 +0500

    Add files via upload

---
+++ Калькулятор/script.js
@@ -0,0 +1,568 @@
+b1.onclick=function(){
+    if(plus==true && minus==true && um==true && div==true){
+    document.getElementById('inp-rez').innerHTML=document.getElementById('inp-rez').textContent+1
+    a=document.getElementById('inp-rez').textContent;
+    }
+    else {  
+        if(z==1)
+        { 
+        b=String(b);
+        b=b+"1";
+        b=Number(b);
+        s=a+"+"+b;
+        document.getElementById('inp-rez').textContent=s;
+        }
+        if(z==2)
+        { 
+        b=String(b);
+        b=b+"1";
+        b=Number(b);
+        s=a+"-"+b;
+        document.getElementById('inp-rez').textContent=s;
+        }
+        if(z==3)
+        { 
+        b=String(b);
+        b=b+"1";
+        b=Number(b);
+        s=a+"*"+b;
+        document.getElementById('inp-rez').textContent=s;
+        }
+        if(z==4)
+        { 
+        b=String(b);
+        b=b+"1";
+        b=Number(b);
+        s=a+"/"+b;
+        document.getElementById('inp-rez').textContent=s;
+        }
+    }
+    // if(plus==true){
+    //     document.getElementById('inp-rez').innerHTML=document.getElementById('inp-rez').textContent+1
+    //     a=document.getElementById('inp-rez').textContent;
+    //     }
+    // else{   
+    //         b=String(b);
+    //         b=b+"1";
+    //         b=Number(b);
+    //         s=a+"+"+b;
+    //         document.getElementById('inp-rez').textContent=s;
+    //     }
+}
+b2.onclick=function(){
+   if(plus==true && minus==true && um==true && div==true){
+    document.getElementById('inp-rez').innerHTML=document.getElementById('inp-rez').textContent+2
+    a=document.getElementById('inp-rez').textContent;
+    }
+    else {  
+        if(z==1)
+        { 
+        b=String(b);
+        b=b+"2";
+        b=Number(b);
+        s=a+"+"+b;
+        document.getElementById('inp-rez').textContent=s;
+        }
+        if(z==2)
+        { 
+        b=String(b);
+        b=b+"2";
+        b=Number(b);
+        s=a+"-"+b;
+        document.getElementById('inp-rez').textContent=s;
+        }
+        if(z==3)
+        { 
+        b=String(b);
+        b=b+"2";
+        b=Number(b);
+        s=a+"*"+b;
+        document.getElementById('inp-rez').textContent=s;
+        }
+        if(z==4)
+        { 
+        b=String(b);
+        b=b+"2";
+        b=Number(b);
+        s=a+"/"+b;
+        document.getElementById('inp-rez').textContent=s;
+        }
+    }
+}
+b3.onclick=function(){
+   if(plus==true && minus==true && um==true && div==true){
+    document.getElementById('inp-rez').innerHTML=document.getElementById('inp-rez').textContent+3
+    a=document.getElementById('inp-rez').textContent;
+    }
+    else {  
+        if(z==1)
+        { 
+        b=String(b);
+        b=b+"3";
+        b=Number(b);
+        s=a+"+"+b;
+        document.getElementById('inp-rez').textContent=s;
+        }
+        if(z==2)
+        { 
+        b=String(b);
+        b=b+"3";
+        b=Number(b);
+        s=a+"-"+b;
+        document.getElementById('inp-rez').textContent=s;
+        }
+        if(z==3)
+        { 
+        b=String(b);
+        b=b+"3";
+        b=Number(b);
+        s=a+"*"+b;
+        document.getElementById('inp-rez').textContent=s;
+        }
+        if(z==4)
+        { 
+        b=String(b);
+        b=b+"3";
+        b=Number(b);
+        s=a+"/"+b;
+        document.getElementById('inp-rez').textContent=s;
+        }
+    }
+}
+b4.onclick=function(){
+  if(plus==true && minus==true && um==true && div==true){
+    document.getElementById('inp-rez').innerHTML=document.getElementById('inp-rez').textContent+4
+    a=document.getElementById('inp-rez').textContent;
+    }
+    else {  
+        if(z==1)
+        { 
+        b=String(b);
+        b=b+"4";
+        b=Number(b);
+        s=a+"+"+b;
+        document.getElementById('inp-rez').textContent=s;
+        }
+        if(z==2)
+        { 
+        b=String(b);
+        b=b+"4";
+        b=Number(b);
+        s=a+"-"+b;
+        document.getElementById('inp-rez').textContent=s;
+        }
+        if(z==3)
+        { 
+        b=String(b);
+        b=b+"4";
+        b=Number(b);
+        s=a+"*"+b;
+        document.getElementById('inp-rez').textContent=s;
+        }
+        if(z==4)
+        { 
+        b=String(b);
+        b=b+"4";
+        b=Number(b);
+        s=a+"/"+b;
+        document.getElementById('inp-rez').textContent=s;
+        }
+    }
+}
+b5.onclick=function(){
+   if(plus==true && minus==true && um==true && div==true){
+    document.getElementById('inp-rez').innerHTML=document.getElementById('inp-rez').textContent+5
+    a=document.getElementById('inp-rez').textContent;
+    }
+    else {  
+        if(z==1)
+        { 
+        b=String(b);
+        b=b+"5";
+        b=Number(b);
+        s=a+"+"+b;
+        document.getElementById('inp-rez').textContent=s;
+        }
+        if(z==2)
+        { 
+        b=String(b);
+        b=b+"5";
+        b=Number(b);
+        s=a+"-"+b;
+        document.getElementById('inp-rez').textContent=s;
+        }
+        if(z==3)
+        { 
+        b=String(b);
+        b=b+"5";
+        b=Number(b);
+        s=a+"*"+b;
+        document.getElementById('inp-rez').textContent=s;
+        }
+        if(z==4)
+        { 
+        b=String(b);
+        b=b+"5";
+        b=Number(b);
+        s=a+"/"+b;
+        document.getElementById('inp-rez').textContent=s;
+        }
+    }
+}
+b6.onclick=function(){
+  if(plus==true && minus==true && um==true && div==true){
+    document.getElementById('inp-rez').innerHTML=document.getElementById('inp-rez').textContent+6
+    a=document.getElementById('inp-rez').textContent;
+    }
+    else {  
+        if(z==1)
+        { 
+        b=String(b);
+        b=b+"6";
+        b=Number(b);
+        s=a+"+"+b;
+        document.getElementById('inp-rez').textContent=s;
+        }
+        if(z==2)
+        { 
+        b=String(b);
+        b=b+"6";
+        b=Number(b);
+        s=a+"-"+b;
+        document.getElementById('inp-rez').textContent=s;
+        }
+        if(z==3)
+        { 
+        b=String(b);
+        b=b+"6";
+        b=Number(b);
+        s=a+"*"+b;
+        document.getElementById('inp-rez').textContent=s;
+        }
+        if(z==4)
+        { 
+        b=String(b);
+        b=b+"6";
+        b=Number(b);
+        s=a+"/"+b;
+        document.getElementById('inp-rez').textContent=s;
+        }
+    }
+}
+b7.onclick=function(){
+   if(plus==true && minus==true && um==true && div==true){
+    document.getElementById('inp-rez').innerHTML=document.getElementById('inp-rez').textContent+7
+    a=document.getElementById('inp-rez').textContent;
+    }
+    else {  
+        if(z==1)
+        { 
+        b=String(b);
+        b=b+"7";
+        b=Number(b);
+        s=a+"+"+b;
+        document.getElementById('inp-rez').textContent=s;
+        }
+        if(z==2)
+        { 
+        b=String(b);
+        b=b+"7";
+        b=Number(b);
+        s=a+"-"+b;
+        document.getElementById('inp-rez').textContent=s;
+        }
+        if(z==3)
+        { 
+        b=String(b);
+        b=b+"7";
+        b=Number(b);
+        s=a+"*"+b;
+        document.getElementById('inp-rez').textContent=s;
+        }
+        if(z==4)
+        { 
+        b=String(b);
+        b=b+"7";
+        b=Number(b);
+        s=a+"/"+b;
+        document.getElementById('inp-rez').textContent=s;
+        }
+    }
+}
+b8.onclick=function(){
+  if(plus==true && minus==true && um==true && div==true){
+    document.getElementById('inp-rez').innerHTML=document.getElementById('inp-rez').textContent+8
+    a=document.getElementById('inp-rez').textContent;
+    }
+    else {  
+        if(z==1)
+        { 
+        b=String(b);
+        b=b+"8";
+        b=Number(b);
+        s=a+"+"+b;
+        document.getElementById('inp-rez').textContent=s;
+        }
+        if(z==2)
+        { 
+        b=String(b);
+        b=b+"8";
+        b=Number(b);
+        s=a+"-"+b;
+        document.getElementById('inp-rez').textContent=s;
+        }
+        if(z==3)
+        { 
+        b=String(b);
+        b=b+"8";
+        b=Number(b);
+        s=a+"*"+b;
+        document.getElementById('inp-rez').textContent=s;
+        }
+        if(z==4)
+        { 
+        b=String(b);
+        b=b+"8";
+        b=Number(b);
+        s=a+"/"+b;
+        document.getElementById('inp-rez').textContent=s;
+        }
+    }
+}
+b9.onclick=function(){
+   if(plus==true && minus==true && um==true && div==true){
+    document.getElementById('inp-rez').innerHTML=document.getElementById('inp-rez').textContent+9
+    a=document.getElementById('inp-rez').textContent;
+    }
+    else {  
+        if(z==1)
+        { 
+        b=String(b);
+        b=b+"9";
+        b=Number(b);
+        s=a+"+"+b;
+        document.getElementById('inp-rez').textContent=s;
+        }
+        if(z==2)
+        { 
+        b=String(b);
+        b=b+"9";
+        b=Number(b);
+        s=a+"-"+b;
+        document.getElementById('inp-rez').textContent=s;
+        }
+        if(z==3)
+        { 
+        b=String(b);
+        b=b+"9";
+        b=Number(b);
+        s=a+"*"+b;
+        document.getElementById('inp-rez').textContent=s;
+        }
+        if(z==4)
+        { 
+        b=String(b);
+        b=b+"9";
+        b=Number(b);
+        s=a+"/"+b;
+        document.getElementById('inp-rez').textContent=s;
+        }
+    }
+}
+b0.onclick=function(){
+   if(plus==true && minus==true && um==true && div==true){
+    document.getElementById('inp-rez').innerHTML=document.getElementById('inp-rez').textContent+0
+    a=document.getElementById('inp-rez').textContent;
+    }
+    else {  
+        if(z==1)
+        { 
+        b=String(b);
+        b=b+"0";
+        b=Number(b);
+        s=a+"+"+b;
+        document.getElementById('inp-rez').textContent=s;
+        }
+        if(z==2)
+        { 
+        b=String(b);
+        b=b+"0";
+        b=Number(b);
+        s=a+"-"+b;
+        document.getElementById('inp-rez').textContent=s;
+        }
+        if(z==3)
+        { 
+        b=String(b);
+        b=b+"0";
+        b=Number(b);
+        s=a+"*"+b;
+        document.getElementById('inp-rez').textContent=s;
+        }
+        if(z==4)
+        { 
+        b=String(b);
+        b=b+"0";
+        b=Number(b);
+        s=a+"/"+b;
+        document.getElementById('inp-rez').textContent=s;
+        }
+    }
+}
+let a=0;
+let r=0;
+let b=0;
+let s="";
+let z=0;
+let plus=minus=um=div=true;
+function p(){
+    b=0;
+    minus=um=div=false;
+    if (document.getElementById('inp-rez').textContent=="")
+    {
+        a = 0;
+        // document.getElementById('inp-rez').textContent="0+";
+    }
+    else
+    {
+        a=document.getElementById('inp-rez').textContent;
+    }
+    s=a+"+";
+    document.getElementById('inp-rez').textContent=s;
+    a=Number(a);
+    z=1;
+}
+
+function m(){
+    b=0;
+    plus=um=div=false;
+    if (document.getElementById('inp-rez').textContent=="")
+    {
+        a = 0;
+        // document.getElementById('inp-rez').textContent="0+";
+    }
+    else
+    {
+        a=document.getElementById('inp-rez').textContent;
+    }
+    s=a+"-";
+    document.getElementById('inp-rez').textContent=s;
+    a=Number(a);
+    z=2;
+
+}
+
+function u(){
+    b=0;
+    plus=minus=div=false;
+    if (document.getElementById('inp-rez').textContent=="")
+    {
+        a = 0;
+        // document.getElementById('inp-rez').textContent="0+";
+    }
+    else
+    {
+        a=document.getElementById('inp-rez').textContent;
+    }
+    s=a+"*";
+    document.getElementById('inp-rez').textContent=s;
+    a=Number(a);   
+    z=3;
+
+}
+function d(){
+    b=0;
+    plus=um=minus=false;
+    if (document.getElementById('inp-rez').textContent=="")
+    {
+        a = 0;
+        // document.getElementById('inp-rez').textContent="0+";
+    }
+    else
+    {
+        a=document.getElementById('inp-rez').textContent;
+    }
+    s=a+"/";
+    document.getElementById('inp-rez').textContent=s;
+    a=Number(a);
+    z=4;
+}
+
+bp.onclick=function(){
+    if(plus==true){
+    p();
+    plus= false;
+    }
+}
+
+bm.onclick=function(){
+    if(minus==true){
+        m();
+        minus= false;
+        }
+}
+
+bu.onclick=function(){
+    if(um==true){
+        u();
+        um= false;
+        }
+}
+bd.onclick=function(){
+    if(div==true){
+        d();
+        div= false;
+        }
+}
+
+br.onclick=function(){
+    a=Number(a);
+    b=Number(b);
+    if(z==3)
+        r=a*b;
+    else
+    {
+        if(z==2)
+            r=a-b;
+        else
+        {
+            if(z==4)
+                r=a/b;
+            else
+                r=a+b;
+        }
+    }
+    document.getElementById('inp-rez').textContent=r;
+    a=document.getElementById('inp-rez').textContent;
+    a=Number(a);
+    plus=minus=um=div=true;
+}
+bc.onclick=function(){
+    a=0;
+    b=0;
+    r=0;
+    s="";
+    plus=minus=um=div=true;
+    document.getElementById('inp-rez').textContent="";
+}
+bs.onclick=function(){
+    
+    s=document.getElementById('inp-rez').textContent;
+    s=s.substring(0, s.length - 1);
+    document.getElementById('inp-rez').textContent=s;
+    if(plus==true)
+    {
+        a=String(a);
+        a=a.substring(0, a.length - 1);
+        a=Number(a);
+    }
+    else
+    {
+        b=String(b);
+        b=b.substring(0, b.length - 1);
+        b=Number(b);
+    }
+}
+rez.onclick = function Rez(){
+    let rez = document.getElementById('inp-rez').innerHTML=10
+}
